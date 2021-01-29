723bad67d8de4a3a159f76c8bc4c25817515a0d1
Vinoth6194 <vinothhkumar.cse.s@gmail.com>
2021-01-29 23:13:51 +0530

    firebase configured

---
+++ src/firebase_config.js
@@ -0,0 +1,14 @@
+import firebase from "firebase";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyCF0TuPYBkdVH2cpqLAt2RhTofyxaYEuDs",
+  authDomain: "code-aff-react-fire-crud.firebaseapp.com",
+  databaseURL: "https://code-aff-react-fire-crud-default-rtdb.firebaseio.com",
+  projectId: "code-aff-react-fire-crud",
+  storageBucket: "code-aff-react-fire-crud.appspot.com",
+  messagingSenderId: "913008262645",
+  appId: "1:913008262645:web:a49e0c0a9296747dbf74bc",
+  measurementId: "G-J6CJFB7YF5",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
