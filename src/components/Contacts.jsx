c4acc11d7bbad16f71584d714a477061032921d1
Vinoth6194 <vinothhkumar.cse.s@gmail.com>
2021-01-30 00:39:37 +0530

    contacts from fire rtdb is displayed in the table

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -29,7 +29,30 @@ function Contacts() {
         <div className="col-md-5">
           <ContactForm addOrEdit={addOrEdit} />
         </div>
-        <div className="col-md-7">Contacts list </div>
+        <div className="col-md-7">
+          <table className="table table-borderless table-striped">
+            <thead className="thead-light">
+              <tr>
+                <th>Full Name</th>
+                <th>Mobile</th>
+                <th>Email</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {Object.keys(contactObjects).map((id) => {
+                return (
+                  <tr>
+                    <td>{contactObjects[id].fullName}</td>
+                    <td>{contactObjects[id].mobile}</td>
+                    <td>{contactObjects[id].email}</td>
+                    <td></td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
       </div>
     </>
   );
