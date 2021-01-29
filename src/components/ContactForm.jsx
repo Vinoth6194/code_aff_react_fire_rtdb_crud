f6ae9803c8cbcb53e4d39ec919ce62b3b0e493b2
Vinoth6194 <vinothhkumar.cse.s@gmail.com>
2021-01-29 23:46:35 +0530

    name, mobile and email fields

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,7 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 
 function ContactForm() {
-  return <div>Contact form</div>;
+  const initialFieldValues = {
+    fullName: "",
+    mobile: "",
+    email: "",
+    address: "",
+  };
+
+  const [values, setValues] = useState(initialFieldValues);
+  return (
+    <form autoComplete="off">
+      <div className="form-group input-group">
+        <div className="input-group-prepend">
+          <div className="input-group-text">
+            <i className="fas fa-user"></i>
+          </div>
+        </div>
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Full Name"
+          value={values.fullName}
+          name="fullName"
+        />
+      </div>
+      <div className="form-row">
+        <div className="form-group input-group col-md-6">
+          <div className="input-group-prepend">
+            <div className="input-group-text">
+              <i className="fas fa-mobile-alt"></i>
+            </div>
+          </div>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Mobile"
+            value={values.mobile}
+            name="mobile"
+          />
+        </div>
+        <div className="form-group input-group col-md-6">
+          <div className="input-group-prepend">
+            <div className="input-group-text">
+              <i className="fas fa-envelope"></i>
+            </div>
+          </div>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Email"
+            value={values.email}
+            name="email"
+          />
+        </div>
+      </div>
+    </form>
+  );
 }
 
 export default ContactForm;
